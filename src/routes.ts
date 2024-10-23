f38a52782b6376729a216d9eab8f6139ec54423f
alison-reisen <alison.reisen@outlook.com>
2024-10-22 23:57:09 -0400

    Adiciona autenticação de clientes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/useCases/createCliente/CreateClientController";
+import { AutenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientUseCase = new AutenticateClientController();
 
 //routes.post("/client/", createClientController.handle);
 routes.post("/client/", async (request, response) => {
     await createClientController.handle(request, response);
+});
+
+routes.post("/authenticate/", async (request, response) => {
+    await authenticateClientUseCase.handle(request, response);
 });
 
 
