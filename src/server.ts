817f5e925271233dedabdb98ab6a86cbef984c77
alison-reisen <alison.reisen@outlook.com>
2024-11-02 18:18:29 -0400

    Cria os tratamentos de erros da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 
 const app = express();
@@ -7,10 +8,23 @@ app.use(express.json());
 
 app.use(routes);
 
-//app.get("/", (request, response): any => {
-//  return response.json({
-//    message: "Hello Word",
-//});
-//});
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction): void => {
+        if (err instanceof Error) {
+            response.status(400).json({
+                message: err.message,
+            });
+            return;
+        }
+
+        response.status(500).json({
+            status: "error",
+            message: "Internal server error",
+        });
+
+    }
+);
+
+
 
 app.listen(3000, () => console.log("Server is Running"));
